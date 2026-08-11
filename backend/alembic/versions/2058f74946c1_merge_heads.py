bcd233434bbb8454140d9c5def753b255fe07b43
luciagiampieri <96505@sistemas.frc.utn.edu.ar>
2026-08-11 16:02:08 -0300

    fix: corrección dos head alembic

---
+++ backend/alembic/versions/2058f74946c1_merge_heads.py
@@ -0,0 +1,17 @@
+"""merge heads"""
+
+revision = '2058f74946c1'
+down_revision = ('0020_item_repositorio', 'e1d5f323fbe0')
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
